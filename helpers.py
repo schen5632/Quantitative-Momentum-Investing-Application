a588d2b42dea65a19593919f28c12a4ff700235e
schen5632 <93620334+schen5632@users.noreply.github.com>
2022-09-18 12:05:19 -0400

    Delete helpers.py

--- helpers.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-class Helper_Functions:
-    def test(self, file):
-        stocks = pd.read_csv(file)
-        stocks = stocks[~stocks['Ticker'].isin(['DISCA', 'HFC', 'VIAC', 'WLTW'])]
-        st.write(stocks)
-
